8605fb42ebe781d5732eb3789d2321c2178df40a
Anubhav Chaturvedi <129895858+anubhavcha@users.noreply.github.com>
2024-01-14 23:19:12 +0530

    Update close.py

--- close.py
+++ close.py
@@ -1,9 +1,8 @@
 import random
-from Body.speak import speak
+from Body.Speak import SPEAK
 import pyautogui as ui
 from Data.Command.data import closedlg
 def close():
     dld1 = random.choice(closedlg)
-    speak(dld1)
+    SPEAK(dld1)
     ui.hotkey("alt", "f4")
-
